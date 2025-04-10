cd029f2abef388fc56d3310b01740696c3aedba0
FilipPietryga <filippatrykpietryga@gmail.com>
2025-04-10 04:58:24 +0200

    Introduced job application list view

--- frontend/src/app/job-applications-list/job-applications-list.component.ts
+++ frontend/src/app/job-applications-list/job-applications-list.component.ts
@@ -1,12 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-job-applications-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './job-applications-list.component.html',
   styleUrl: './job-applications-list.component.sass'
 })
 export class JobApplicationsListComponent {
+  jobApplications = [
+    { id: 1, title: 'Software Developer', company: 'Nexi', status: 'Applied' },
+    { id: 2, title: 'Software Developer', company: 'Comarch', status: 'Applied' },
+    { id: 3, title: 'Software Developer', company: 'Euvic :)', status: 'Interviewed' }
+  ];
 
+  constructor() { }
+
+  ngOnInit(): void { }
 }
