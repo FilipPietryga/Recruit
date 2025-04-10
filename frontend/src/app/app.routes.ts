339b3ba303edfa7c64eacaa1ce3355c9bf48b169
FilipPietryga <filippatrykpietryga@gmail.com>
2025-04-10 03:48:56 +0200

    introduced routing

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { JobApplicationsListComponent } from './job-applications-list/job-applications-list.component';
+import { JobApplicationFormComponent } from './job-application-form/job-application-form.component';
+import { JobApplicationDetailsComponent } from './job-application-details/job-application-details.component';
 
-export const routes: Routes = [];
+export const appRoutes: Routes = [
+  { path: '', component: JobApplicationsListComponent },
+  { path: 'job-application-form', component: JobApplicationFormComponent },
+  { path: 'job-application-details/:id', component: JobApplicationDetailsComponent },
+];
